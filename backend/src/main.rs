3186b05e5695f5f9defcf5d26c41eec3431d1edc
Paul Biggar <paul.biggar@gmail.com>
2020-08-31 19:36:44 -0400

    Hook up webserver to execution engine

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,75 +2,90 @@
 #![feature(box_syntax)]
 #![feature(log_syntax)]
 
-use execution_engine::{
-  self, dval, errors, eval, expr::*, ivec, runtime,
+use hyper::{
+  service::{make_service_fn, service_fn},
+  Body, Request, Response, Server,
 };
+use std::{convert::Infallible, net::SocketAddr};
 
-fn main() -> Result<(), errors::Error> {
+use execution_engine::{self, dval, eval, expr::*, ivec, runtime};
+
+fn program() -> Expr {
+  elet("range",
+       esfn("Int", "range", 0, ivec![eint(1), eint(100),]),
+       esfn("List",
+            "map",
+            0,
+            ivec![(evar("range")),
+                  elambda(ivec!["i"],
+                          eif(ebinop(ebinop(evar("i"),
+                                            "Int",
+                                            "%",
+                                            0,
+                                            eint(15)),
+                                     "Int",
+                                     "==",
+                                     0,
+                                     eint(0)),
+                              estr("fizzbuzz"),
+                              eif(ebinop(ebinop(evar("i"),
+                                                "Int",
+                                                "%",
+                                                0,
+                                                eint(5)),
+                                         "Int",
+                                         "==",
+                                         0,
+                                         eint(0)),
+                                  estr("buzz"),
+                                  eif(ebinop(ebinop(evar("i"),
+                                                    "Int",
+                                                    "%",
+                                                    0,
+                                                    eint(3)),
+                                             "Int",
+                                             "==",
+                                             0,
+                                             eint(0)),
+                                      estr("fizz"),
+                                      esfn("Int",
+                                           "toString",
+                                           0,
+                                           ivec![evar("i")])))))]))
+}
+
+async fn run_program(_req: Request<Body>)
+                     -> Result<Response<Body>, Infallible> {
   let tlid = runtime::TLID::TLID(7);
-  let program = elet(
-                     "range",
-                     esfn(
-    "Int",
-    "range",
-    0,
-    ivec![eint(1), eint(100),],
-  ),
-                     esfn(
-    "List",
-    "map",
-    0,
-    ivec![(evar("range")),
-
-              elambda(ivec!["i"],
-               eif(
-                 ebinop(
-                 ebinop(
-                   evar("i"),"Int",
-                   "%", 0,
-                   eint(15)
-                  ), "Int", "==", 0, eint(0)), estr("fizzbuzz"),
-
-                eif(
-                 ebinop(
-                 ebinop(
-                   evar("i"),
-                   "Int",
-                   "%",0,
-                   eint(5)
-                  ), "Int", "==", 0, eint(0)), estr("buzz"),
-
-                eif(
-                 ebinop(
-                 ebinop(
-                   evar("i"),
-                   "Int",
-                   "%", 0,
-                   eint(3)
-                  ), "Int", "==", 0, eint(0)), estr("fizz"), esfn("Int", "toString", 0, ivec![evar("i")])   )
-
-
-                )
-
-                ))  ],
-  ),
-  );
-
   let state =
     eval::ExecState { caller: runtime::Caller::Toplevel(tlid), };
-  let result = eval::run(&state, program);
+  let result = eval::run(&state, program());
   match &*result {
     dval::Dval_::DSpecial(dval::Special::Error(_, err)) => {
-      use std::io::Write;
-      let stderr = &mut ::std::io::stderr();
-      let errmsg = "Error writing to stderr";
-
-      writeln!(stderr, "error: {}", err).expect(errmsg);
-      ::std::process::exit(1);
+      let msg = format!("Error: {}", err);
+      Ok(Response::new(msg.into()))
     }
     _ => {
-      println!("{:?}", result);
-      Ok(())
+      let str = format!("{:?}", result);
+      Ok(Response::new(str.into()))
     }
+  }
+}
+
+#[tokio::main]
+async fn main() {
+  let addr = SocketAddr::from(([127, 0, 0, 1], 8088));
+  // A `Service` is needed for every connection, so this
+  // creates one from our `hello_world` function.
+  let make_svc = make_service_fn(|_conn| async {
+    // service_fn converts our function into a `Service`
+    Ok::<_, Infallible>(service_fn(run_program))
+  });
+
+  let server = Server::bind(&addr).serve(make_svc);
+
+  // Run this server for... forever!
+  if let Err(e) = server.await {
+    eprintln!("server error: {}", e);
   }
 }
